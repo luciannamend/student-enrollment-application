d9e33087cb2bb88a2e7699ca7d78bf9f719a280f
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-12 17:41:25 -0400

    Initial commit

---
+++ src/main/java/com/lucianna/mendonca/studentenrollmentapplication/StudentEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.lucianna.mendonca.studentenrollmentapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEnrollmentApplication.class, args);
+	}
+
+}
