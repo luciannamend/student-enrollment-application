c767743cf2a5933688a06b80ea50858ba75560f8
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-14 15:15:40 -0400

    Implemented enrollment methods and confirmation page

--- src/main/java/com/lucianna/mendonca/studentenrollmentapplication/controller/EnrollmentController.java
+++ src/main/java/com/lucianna/mendonca/studentenrollmentapplication/controller/EnrollmentController.java
@@ -6,13 +6,13 @@ import com.lucianna.mendonca.studentenrollmentapplication.model.Student;
 import com.lucianna.mendonca.studentenrollmentapplication.repository.EnrollmentRepository;
 import com.lucianna.mendonca.studentenrollmentapplication.repository.ProgramRepository;
 import com.lucianna.mendonca.studentenrollmentapplication.repository.StudentRepository;
-import com.lucianna.mendonca.studentenrollmentapplication.service.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -24,29 +24,27 @@ public class EnrollmentController {
     private ProgramRepository programRepository;
     @Autowired
     private EnrollmentRepository enrollmentRepository;
+    @Autowired
+    private StudentRepository studentRepository;
+
 
     @PostMapping("/enroll")
-    public String enrollInProgram(@RequestParam("programCode") Integer programCode,
-                                  @ModelAttribute("student") Student student,
-                                  Model model) {
-        // Fetch the selected program from the database
-        Program selectedProgram = programRepository.findByProgramCode(programCode);
+    public String enrollInProgram(@ModelAttribute ("student") Student student,
+                                  @ModelAttribute("program") Program selectedProgram) {
+
 
         // Create a new enrollment record
         Enrollment enrollment = new Enrollment();
         enrollment.setStudent(student);
         enrollment.setProgram(selectedProgram);
-        enrollment.setStartDate(LocalDate.now()); // Set the current date as the start date
-        enrollment.setAmountPaid(new BigDecimal(100));
+        enrollment.setStartDate(LocalDate.of(2025, 01, 16)); // Set the current date as the start date
+        enrollment.setAmountPaid(selectedProgram.getFee());
 
         // Save the enrollment in the database
         enrollmentRepository.save(enrollment);
 
-        // Add success message to the model
-        model.addAttribute("message", "You have successfully enrolled in " +
-                selectedProgram.getProgramName());
-
         // Return the view with the enrollment confirmation
-        return "enrollmentConfirmation"; // A view that confirms the enrollment
+        return "enrollmentcompleted"; // A view that confirms the enrollment
     }
 }
+
